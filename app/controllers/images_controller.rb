a6be5bfa67c88825e3be6bb00c3789258226b017
mapimopi <mapimopipi@gmail.com>
2013-06-22 21:52:54 +0400

    authenticity fix

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ require "#{Rails.root}/lib/tasks/imgur.rb"
 
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
-  #skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
   protect_from_forgery :except => :create
   
   # GET / /images
